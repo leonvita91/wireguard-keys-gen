487c7837e1069e323e472b1ee22a738e83285985
leon <leonvita91@gmail.com>
2023-01-02 03:28:59 +0000

    add input & checking & insert into database correct

--- data.py
+++ data.py
@@ -30,14 +30,14 @@ connect.execute("""CREATE TABLE IF NOT EXISTS VPN (
 
 # user input username & ip.
 def Vpn_input():
-    #read keys
+    #define global var
+    global user_name,user_ips,user_public_key,user_private_key,date,time
+    #Generate keys
     subprocess.run("wg genkey | tee privatekey | wg pubkey > publickey",
     shell=True,capture_output=True
     )
     #convert keys to txt file
     subprocess.run("mv publickey publickey.txt && mv privatekey privatekey.txt",shell=True)
-    #global var
-    #global user_name,user_ips,username_len_input,userip_len_input
     #format date and time
     now = datetime.now()
     dates = now.strftime("%Y-%m-%d")
@@ -47,7 +47,7 @@ def Vpn_input():
         public = pk.read()
     with open("privatekey.txt","r") as prk:
         private = prk.read()
-    #variables store keys and data&time
+    #variables stores keys,date,time
     user_public_key = public
     user_private_key = private
     date = dates
@@ -55,42 +55,25 @@ def Vpn_input():
     #User_name_input & user_input ip
     user_name = input(str("Insert username: ")) #15
     user_ips = input(str("Insert IP:"))# 20
+    # add the lenght inside list.
     username_len_input = len(user_name)
     userip_len_input = len(user_ips)
-
-def user_enter(user_name,user_ips,username_len_input,userip_len_input):
     #check if the username & ip are not out of range.
-    while int(username_len_input) > 10 or int(userip_len_input) > 15:
-        subprocess.run('clear')
+    while username_len_input > 10 or userip_len_input > 15:
         print(colors.bold,colors.red,'WARNING!!',colors.end)
         print('You have entered:')
         print('Username_Charactor=',username_len_input,'IP_length=',userip_len_input)
         print(colors.bold,colors.cyan,'\nUSER_NAME 10 CHARACTOR MAX && IPs 15 DIGIT MAX.',colors.end)
-        user_name = input(str("Insert username: "))
-        user_ips = input(str("Insert IP:"))
+        #User_name_input & user_input ip
+        user_name = input(str("Insert username: ")) #15
+        user_ips = input(str("Insert IP:")) #20
+        # add the lenght inside list.
         username_len_input = len(user_name)
         userip_len_input = len(user_ips)
-    #revers function from Checking user & ip lenght
-    user_enter(user_name,user_ips,username_len_input,userip_len_input)
-    #revese function from checking
-    checking(user_name)
-    # reverse function from add_ips function
-    add_ips(user_name,user_ips,
-    user_public_key,user_private_key,
-    date,time
-    )
+    # end function.
 
-# def checking(user_name):
-#     connect.execute("SELECT user_vpn FROM VPN")
-#     search = connect.fetchall()
-#     for searchs in search:
-#         user = "".join(searchs)
-#         if user == 'leon':
-
-        
-#add users & ips & keys & date & time into Database
-def add_ips(user_name,user_ips,user_public_key,user_private_key,date,time):
-    #insert values to VPN TABLE
+def add_ips():
+    #insert the values to VPN TABLE
     connect.execute("INSERT INTO VPN VALUES (?,?,?,?,?,?)",
     (
     (user_name),
@@ -100,23 +83,40 @@ def add_ips(user_name,user_ips,user_public_key,user_private_key,date,time):
     ('Date: ' + date),
     ('Time: ' + time)
     ))
+    create.commit() #Commit into db
+    subprocess.run("rm privatekey.txt publickey.txt",shell=True) #delete the keys
+#end function.
 
-    #Commit into db
-    create.commit()
-    #delete the keys
-    subprocess.run("rm privatekey.txt publickey.txt",shell=True)
+def checking():
+    connect.execute("SELECT user_vpn FROM VPN")
+    search = connect.fetchall()
+    for searchs in search:
+        user = "".join(searchs) # convert from tuple to string
+        if user == user_name:
+            print('name exsit')
+            Vpn_input() #if the name exist it will return to Vpn_input function again.
+            checking() # checking again
+        else:
+            print('done')
+            break
+
+        
+#add users & ips & keys & date & time into Database
     
     
+#Call functions
+Vpn_input() # call input function
+checking() # call checking function
+add_ips() # call add to database function
+
 
 
 
 #-----------------------------------------------------
 
-# #call fucn
-Vpn_input()
 
 #Close the db after finishing
-create.close()
+#create.close()
     #Notes:
     #if you want to search something in specific table and column
     #connect.execute("SELECT * FROM VPN WHERE user_vpn == (?)",(user_name,))
