6c2d4caa7b830b8ab1c4352bd616db99176fafbd
leon <leonvita91@gmail.com>
2023-01-06 12:36:41 +0000

    add more error handle

--- main.py
+++ main.py
@@ -14,30 +14,50 @@ try:
         add.art.creator()
         add.art.section()
         print('>.1 Create new user.\n>.2 Check exsiting username & IP\n>.3 menu\n>.4 Exit')
-        dec = int(input('Choose one of the Options: '))
-        if dec == 1:
-            add.Vpn_input()
-            add.check_username()
-            add.check_ip()
-            add.insert_data()
-        elif dec == 2:
-            add.Vpn_input()
-            add.check_username()
-            add.check_ip()
-            add.art.not_exis()
-        elif dec == 3:
+        try:
+            dec = int(input('Choose one of the Options: '))
+            if dec == 1:
+                add.Vpn_input()
+                add.check_username()
+                add.check_ip()
+                add.insert_data()
+                print("Username && IP With keys >> has been created.")
+                sleep(0.5)
+                print('Would you like to add another user ??')
+                another = str(input('/y|n/ : '))
+                another = 'y' and create()
+                another = 'n' and print('Back to menu.....')
+            elif dec == 2:
+                add.Vpn_input()
+                add.check_username()
+                add.check_ip()
+                add.art.not_exis()
+            elif dec == 3:
+                menus()
+            elif dec == 4:
+                exit()
+            add.art.done()
             menus()
-        elif dec == 4:
-            exit()
-        else:
-            exit()
-        add.art.done()
-
-        sleep(1)
-        # menus()
+            sleep(1)
+        except Exception:
+            print('Bad input try on of numbers above !!')
+            sleep(2)
+            menus()
 
     def call():
-        pass
+        add.clean()
+        add.art.creator()
+        add.art.fech_art()
+        print('>.1 Fetch ALL.\n>.2 Fetch username\n>.3 Fetch IPs\n>.4 Fetch Public key.\n>.5 Fetch Private key.\n>.6 Fetch Users & IPs.\n>.7 Fetch username & IPs & Public key.')
+        dec = int(input('Choose one of the Options: '))
+        dec == 1 and fetch.fetch_all()
+        dec == 2 and fetch.fetch_username()
+        dec == 3 and fetch.fetch_IP()
+        dec == 4 and fetch.fetch_public_key()
+        dec == 5 and fetch.fetch_private_key()
+        dec == 6 and fetch.fetch_user_and_ip()
+        dec == 7 and fetch.fetch_user_ip_public_key()
+    
     def remove():
         pass
     
@@ -48,17 +68,10 @@ try:
         print('>.1 Create new user.\n>.2 Fetch Info\n>.3 Delete Info\n>.4 Exit')
         # user decision:
         dec = int(input('Choose one of the Options: '))
-        if dec == 1:
-            create()
-        elif dec == 2:
-            call()
-        elif dec == 3:
-            remove()
-        elif dec == 4:
-            print('Good Bye....')
-            exit()
-        else:
-            print('error')
+        dec == 1 and create()
+        dec == 2 and call()
+        dec == 3 and remove()
+        dec == 4 and print('Good Bye....') , exit()
     menus()
 
 
