eeab99e81bc0e12093d19a95f8677cd204e59712
leon <leonvita91@gmail.com>
2023-01-07 22:42:28 +0000

    trying to add search option

--- search.py
+++ search.py
@@ -1,8 +1,27 @@
 import add
 
 class Searching():
-    def search(self ):
-        add.connect.execute("SELECT rowid , user_vpn  FROM VPN WHERE public_key like '%Rc%'")
-        test = add.connect.fetchall()
-        for x in test:
+    def __init__(self):
+        self.id = int(input('ID Row searching: '))
+        self.take = add.connect.execute(f"SELECT user_vpn FROM VPN Where rowid = (?)",(self.id,))
+        self.call = add.connect.fetchall()
+        # self.ser = ser
+        self.loop()
+        self.calling()
+    
+    def loop(self):
+        # add.connect.execute(f"SELECT rowid ,  user_vpn FROM VPN WHERE user_vpn like (?)",(self.ser,))
+        for x in self.call:
+            print(x)
+        print('stop')
+    
+    def calling(self):
+        self.id
+        self.take
+        self.call
+        for s in self.call:
+            print(s,'second search')
+out = Searching()
+
+
 
